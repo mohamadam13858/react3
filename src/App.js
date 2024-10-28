c3b3c02b06d7658cf241247e11cad884b2533b43
mohamad <mh711748@gmail.com>
2024-10-28 18:36:48 +0330

    form3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import TopForm from "./TopForm";
-import TaskItems from "./Taskitems";
+import TaskItems from "./Taskitems"; 
+import { taskContext } from "./taskContext";
+
 
 
 const App = () => {
+
+  const [taskItems, setTaskItems] = useState([
+    {
+      id: 1,
+      title: "کار شماره 1",
+      done:false
+
+    },
+    {
+      id: 4,
+      title: "کار شماره 2",
+      done:true
+    },
+    {
+      id: 3,
+      title: "کار شماره 3",
+      done:false
+    }
+  ])
+
   return (
     <div className="container w-100 h-100 p-2">
-      <div className="row h-100 justify-content-center align-item-start">
-        <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
-          <TopForm />
-          <TaskItems />
+      <div className="row h-100 justify-content-center
+      align-align-item-start">
+        <div className="col-12 col-md-8 col-lg-6
+        bg-light shadow rounded-3 p-3 h_fit">
+          <taskContext.Provider value={{
+            taskItems,
+            setTaskItems
+          }}>
+            <TopForm />
+            <TaskItems />
+          </taskContext.Provider>
         </div>
       </div>
     </div>
--- src/Taskitems.jsx
+++ src/Taskitems.jsx
@@ -1,45 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import { taskContext } from "./taskContext";
 
-const TaskItems = ()=>{
-    return(
+const TaskItems = () => {
+    const { taskItems, setTaskItems } = useContext(taskContext)
+
+    return (
         <ul className=" list-group m-0 p-0 mt-2">
-        <li className=" list-group-item  d-flex
-        justify-content-between">
-          کار شماره یک
-          <span>
-            <i className="me-3 pointer fas
-            fa-check text-success
-            transition_200
-            text_hover_shadow"></i>
-            <i className="me-3 pointer fas
-            fa-times text-warning
-            transition_200
-            text_hover_shadow"></i>
-            <i className="me-3 pointer fas
-            fa-trash text-danger
-            transition_200
-            text_hover_shadow"></i>
-          </span>
-        </li>
-        <li className=" list-group-item  d-flex
-        justify-content-between">
-          کار شماره دو
-          <span>
-            <i className=" me-3 pointer fas
-            fa-check text-success
-            transition_200
-            text_hover_shadow"></i>
-            <i className="me-3 pointer fas
-            fa-times text-warning
-            transition_200
-            text_hover_shadow"></i>
-            <i className="me-3 pointer fas
-            fa-trash text-danger
-            transition_200
-            text_hover_shadow bi bi-backpack4-fill"></i>
-          </span>
-        </li>
-      </ul>
+            {
+                taskItems.map(t => (
+                    <li className={`list-group-item  d-flex
+                        justify-content-between ${t.done ? "list-group-item-success" : ""}`}>
+                        {t.title}
+                        <span>
+                            {
+                                t.done ? (
+                                    <i className="me-3 pointer fas
+                                     fa-times text-warning
+                                     transition_200
+                                     text_hover_shadow"></i>
+
+                                ) : (
+
+                                    <i className="me-3 pointer fas
+                                     fa-check text-success
+                                     transition_200
+                                     text_hover_shadow"></i>
+                                )
+                            }
+                            <i className="me-3 pointer fas
+                             fa-trash text-danger
+                             transition_200
+                            text_hover_shadow"></i>
+                        </span>
+                    </li>
+
+                ))
+            }
+        </ul>
     )
 }
 export default TaskItems;
